718dfd0f10e9e34940c75a8513fd92d9a1999cf4
gtitov <g.titov@marine-rc.ru>
2021-05-12 18:12:06 +0300

    sbd processing

--- getsbd.sh
+++ getsbd.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 fdm -v fetch
-find ~/Mail/attachments -type f -iname '*.sbd*' -size 32c -exec bin2csv {}>>$(date --iso-8601=minute).csv \; -delete 
+sudo /usr/bin/find "/home/root-gy/Mail/marlin" -type f -iname '*.sbd' -size 32c -exec /usr/bin/python3 "/home/root-gy/Mail/readmailsbd/unpack_weather.py" {} "/var/www/marlin" \; -exec /usr/bin/mv {} /home/root-gy/Mail/marlin-archive \;
+#sudo /usr/bin/find '/home/root-gy/Mail/marlin' -type f -iname '*.sbd' -size 32c -exec /usr/bin/python3 '/home/root-gy/Mail/readmailsbd/unpack_weather.py' {} '/var/www/marlin' \; -delete
+#sudo find marlin -type f -iname '*.sbd' -size 32c -exec /home/root-gy/Mail/readmailsbd/unpack_weather.py {} "/var/www/marlin" \;
+#find ~/Mail/attachments -type f -iname '*.sbd' -size 32c -exec bin2csv {}>>$(date --iso-8601=minute).csv \; -delete 
